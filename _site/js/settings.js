eb23ea96a3caa557f2f6bb48fdf4f139327118e9
datashaman <marlinf@datashaman.com>
2015-04-26 11:25:16 +0200

    Upload bower_components as well

---
+++ _site/js/settings.js
@@ -0,0 +1,42 @@
+function refreshListGroup(tableView, input) {
+    $(tableView + ' .list-group-item').removeClass('active');
+
+    var value = $(input).val();
+
+    if(value) {
+        _.find($(tableView + ' .list-group-item').get(), function(cell) {
+            if($(cell).html() == value) {
+                $(cell).addClass('active');
+                return true;
+            }
+        });
+    }
+}
+
+function initListGroup(tableView, input, key, def) {
+    $(input).val(store.get(key, def));
+
+    refreshListGroup(tableView, input);
+
+    $(tableView + ' .list-group-item').click(function() {
+        $(input).val($(this).html());
+        refreshListGroup(tableView, input);
+    });
+}
+
+jQuery(document).ready(function($) {
+    $('#table_id').val(store('table_id'));
+    $('#api_key').val(store('api_key'));
+
+    initListGroup('#select-climate', '#climate', 'climate', 'Dry Summer - Wet Winter');
+
+    $('#settings').submit(function() {
+        var elements = $('#settings').serializeArray();
+        var values = _.zipObject(_.map(elements, function(element) {
+            return [ element.name, element.value ];
+        }));
+        store(values);
+        window.location.href = '/';
+        return false;
+    });
+});
