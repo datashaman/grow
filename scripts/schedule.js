94dc88cfde21a88445a65a524fcf211e52dec12d
datashaman <marlinf@datashaman.com>
2015-05-02 15:31:59 +0200

    Update 2015-05-02T13:31:50.120Z

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -144,7 +144,11 @@ Schedule = React.createClass({
               "target": "_blank",
               "className": "pull-right wikipedia",
               "href": wikipedia
-            }, "\x3Cimg width=\"20\" height=\"20\" src=\"", config.site.baseurl + "/images/icons/wikipedia.png", "\" \x2F\x3E") : ''), React.createElement("span", {
+            }, React.createElement("img", {
+              "width": "20",
+              "height": "20",
+              "src": config.site.baseurl + "/images/icons/wikipedia.png"
+            })) : ''), React.createElement("span", {
               "className": "instruction pull-right"
             }, config.instructions[instruction]), React.createElement("span", {
               "className": 'glyphicon glyphicon-' + _this.getGlyphiconByType(type),
